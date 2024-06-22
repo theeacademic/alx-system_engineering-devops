83f51a4ab364f6627c256b817d01b8ca546950f5
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-06-22 14:41:33 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,19 @@
-Notice: Compiled catalog for e514b399d69d.ec2.internal in environment production in 0.02 seconds
-Notice: /Stage[main]/Main/Exec[fix-wordpress]/returns: executed successfully
-Notice: Finished catalog run in 0.08 seconds
-root@e514b399d69d:~# curl -sI 127.0.0.1:80
-root@e514b399d69d:~#
-HTTP/1.1 200 OK
-Date: Fri, 24 Mar 2017 07:11:52 GMT
-Server: Apache/2.4.7 (Ubuntu)
-X-Powered-By: PHP/5.5.9-1ubuntu4.21
-Link: <http://127.0.0.1/?rest_route=/>; rel="https://api.w.org/"
-Content-Type: text/html; charset=UTF-8
+# 0-strace_is_your_friend.pp
+exec { 'create_missing_config':
+  command => '/bin/echo "Configuration content" > /etc/apache2/sites-available/missing-config.conf',
+  creates => '/etc/apache2/sites-available/missing-config.conf',
+}
 
-root@e514b399d69d:~# curl -s 127.0.0.1:80 | grep Holberton
-<title>Holberton &#8211; Just another WordPress site</title>
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
-        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
-                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
-        <p>Yet another bug by a Holberton student</p>
+file { 'enable_config':
+  path    => '/etc/apache2/sites-enabled/missing-config.conf',
+  ensure  => 'link',
+  target  => '/etc/apache2/sites-available/missing-config.conf',
+  require => Exec['create_missing_config'],
+}
+
+service { 'apache2':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['enable_config'],
+}
+

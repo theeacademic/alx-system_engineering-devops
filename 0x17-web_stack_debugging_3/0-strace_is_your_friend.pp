5feea93fc4904d6d61216abb6aacb2ecbe23ea7b
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-06-22 14:50:10 +0000

    apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,12 @@
-# 0-strace_is_your_friend.pp
+# Puppet Manifest for configuring Apache and ensuring a specific configuration file exists
+
+# Exec resource to create a missing Apache config file
 exec { 'create_missing_config':
   command => '/bin/echo "Configuration content" > /etc/apache2/sites-available/missing-config.conf',
   creates => '/etc/apache2/sites-available/missing-config.conf',
 }
 
+# File resource to enable the created config file
 file { 'enable_config':
   path    => '/etc/apache2/sites-enabled/missing-config.conf',
   ensure  => 'link',
@@ -11,9 +14,11 @@ file { 'enable_config':
   require => Exec['create_missing_config'],
 }
 
+# Service resource to ensure Apache is running and enabled
 service { 'apache2':
   ensure    => 'running',
   enable    => true,
   subscribe => File['enable_config'],
+  provider  => 'systemd',  # Specify systemd as the service provider if using systemd-based systems
 }
 
